17d1936eedcb60de22e3fa45680ab3419a1839f2
PsyhoZOOM <PsyhoZOOM@192.168.0.54>
2017-11-20 15:57:29 +0100

    FAKTURE auto generate

--- src/main/java/net/yuvideo/jgemstone/server/classes/FaktureFunct.java
+++ src/main/java/net/yuvideo/jgemstone/server/classes/FaktureFunct.java
@@ -10,161 +10,149 @@ import java.time.format.DateTimeFormatter;
 /**
  * Created by PsyhoZOOM@gmail.com on 11/3/17.
  */
-
-
 public class FaktureFunct {
 
-    private final LocalDate zaGodinu;
-    private final String operName;
-    public boolean hasFirma = false;
-    private int userID;
-    private database db;
-    private int brFakture;
-    private int idFakture;
-    private SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-    private SimpleDateFormat dfYM = new SimpleDateFormat("yyyy-MM");
+	private final LocalDate zaGodinu;
+	private final String operName;
+	public boolean hasFirma = false;
+	private int userID;
+	private database db;
+	private int brFakture;
+	private int idFakture;
+	private SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+	private SimpleDateFormat dfYM = new SimpleDateFormat("yyyy-MM");
 
-    /**
-     * /**
-     * <p>
-     * 1.      Proveriti da li je korisnik userID firma <br>
-     * 1.1     Ako ne, return; <br>
-     * 1.2     Ako da.. <br>
-     * 1.2.1       Pogledati dali ima fakture za ovu godinu,
-     * ako nema
-     * napraviti novu fakturu za ovu godinu sa brojem 1
-     * importovati data
-     * ako ima
-     * uzeti id fakture i importovati data
-     * </p>
-     * @param userID ; id korisnika
-     * @param zaGodinu ; godina-broj fakture
-     * @param operater
-     * @param db ;     database class
-     */
-    public FaktureFunct(int userID, LocalDate zaGodinu, String operater, database db) {
-        this.db = db;
-        this.userID = userID;
-        this.zaGodinu = zaGodinu;
-        this.operName = operater;
-        checkFirma();
-    }
+	/**
+	 * /
+	 **
+	 * <p>
+	 * 1. Proveriti da li je korisnik userID firma <br>
+	 * 1.1 Ako ne, return; <br>
+	 * 1.2 Ako da.. <br>
+	 * 1.2.1 Pogledati dali ima fakture za ovu godinu, ako nema napraviti novu
+	 * fakturu za ovu godinu sa brojem 1 importovati data ako ima uzeti id
+	 * fakture i importovati data
+	 * </p>
+	 *
+	 * @param userID ; id korisnika
+	 * @param zaGodinu ; godina-broj fakture
+	 * @param operater
+	 * @param db ; database class
+	 */
+	public FaktureFunct(int userID, LocalDate zaGodinu, String operater, database db) {
+		this.db = db;
+		this.userID = userID;
+		this.zaGodinu = zaGodinu;
+		this.operName = operater;
+		checkFirma();
+	}
 
+	/**
+	 * provera da li korisnik ima firmu ako nema hasFirma = false i zavrsavamo
+	 * ako ima firmu pravimo fakturu
+	 */
+	private void checkFirma() {
+		PreparedStatement ps;
+		ResultSet rs;
+		String query = "SELECT * FROM users WHERE id=? AND firma=true";
 
-    /**
-     * provera da li korisnik ima firmu
-     * ako nema hasFirma = false i zavrsavamo
-     * ako ima firmu pravimo fakturu
-     */
+		try {
+			ps = db.conn.prepareStatement(query);
+			ps.setInt(1, userID);
+			rs = ps.executeQuery();
 
-    private void checkFirma() {
-        PreparedStatement ps;
-        ResultSet rs;
-        String query = "SELECT * FROM users WHERE id=?";
+			if (rs.isBeforeFirst()) {
+				rs.next();
+				hasFirma = true;
 
-        try {
-            ps = db.conn.prepareStatement(query);
-            ps.setInt(1, userID);
-            rs = ps.executeQuery();
+				//provera faktura za ovu godinu
+				brFakture = checkFakturaExist();
 
-            if (rs.isBeforeFirst()) {
-                rs.next();
-                hasFirma = true;
+			} else {
+				//ako nema firmu zatvaramo sql i zavrsavamo sa fakturama
+				rs.close();
+				ps.close();
+				hasFirma = false;
+			}
 
-                //provera faktura za ovu godinu
-                brFakture = checkFakturaExist();
-                
-            } else {
-                //ako nema firmu zatvaramo sql i zavrsavamo sa fakturama
-                rs.close();
-                ps.close();
-                hasFirma = false;
-            }
+			ps.close();
+			rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
-            ps.close();
-            rs.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+	private void insertFaktureData(ResultSet rs) {
+		PreparedStatement ps;
+		String query = "INSERT INTO faktureData (br, naziv, jedMere, kolicina, "
+				+ "cenaBezPDV, pdv,  operater, userID, datum, godina, mesec ) "
+				+ "VALUES "
+				+ "(?,?,?,?,?,?,?,?,?,?,?)";
 
-    private void insertFaktura() {
-        PreparedStatement ps;
-        String query = "INSERT INTO fakture (userID, brojFakture, datum, dateCreated)" +
-                "VALUES" +
-                "(?,?,?,?)";
+		try {
+			ps = db.conn.prepareStatement(query);
+			ps.setString(1, String.valueOf(brFakture));
+			ps.setString(2, rs.getString("nazivPaketa"));
+			ps.setString(3, "kom.");
+			ps.setDouble(4, 1);
+			ps.setDouble(5, rs.getDouble("cena"));
+			ps.setDouble(6, rs.getDouble("PDV"));
+			ps.setString(7, operName);
+			ps.setInt(8, rs.getInt("userID"));
+			ps.setString(9, this.zaGodinu.format(DateTimeFormatter.ofPattern("yyyy-MM")));
+			ps.setString(10, this.zaGodinu.format(DateTimeFormatter.ofPattern("yyyy")));
+			ps.setString(11, this.zaGodinu.format(DateTimeFormatter.ofPattern("MM")));
 
-        try {
-            ps = db.conn.prepareStatement(query);
-            ps.setInt(1, userID);
-            ps.setString(2, String.valueOf(brFakture));
-            ps.setString(3, LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-            ps.setString(4, LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM")));
-            ps.executeUpdate();
-            ps.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+			ps.executeUpdate();
 
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
 
-    }
+	private int checkFakturaExist() {
+		int broj = 1;
+		PreparedStatement ps;
+		ResultSet rs;
+		String query = "SELECT count(distinct mesec), mesec, br FROM faktureData WHERE userID=? AND godina=? group by mesec asc";
 
-    private void insertFaktureData(ResultSet rs) {
-        PreparedStatement ps;
-	String query = "INSERT INTO faktureData (br, naziv, jedMere, kolicina, "
-		+ "cenaBezPDV, pdv, idFakture, operater) VALUES(?,?,?,?,?,?,?,?)";
+		try {
+			ps = db.conn.prepareStatement(query);
+			ps.setInt(1, userID);
+			ps.setString(2, this.zaGodinu.format(DateTimeFormatter.ofPattern("yyyy")));
+			rs = ps.executeQuery();
 
-        try {
-            ps = db.conn.prepareStatement(query);
-            ps.setString(1, String.valueOf(brFakture));
-            ps.setString(2, rs.getString("nazivPaketa"));
-            ps.setString(3, "kom.");
-            ps.setDouble(4, 1);
-            ps.setDouble(5, rs.getDouble("cenaBezPDV"));
-            ps.setDouble(6, rs.getDouble("pdv"));
-            ps.setInt(7, rs.getInt("idFakture"));
-            ps.setString(8, operName);
+			if (rs.isBeforeFirst()) {
+				while (rs.next()) {
+					if (rs.getInt(1) > 0) {
+						if (rs.getInt(2) == Integer.
+								valueOf(this.zaGodinu.
+										format(DateTimeFormatter.
+												ofPattern("MM")))) {
+							broj = rs.getInt(3);
+						}else if (rs.getInt(2) 
+								< Integer.valueOf(this.zaGodinu.format(DateTimeFormatter.ofPattern("MM")))){
+							broj = rs.getInt(3)+1;
+							
+						}
 
-	    ps.executeQuery();
+					} 
+					
+				}
+			}else{
+				broj = 1;
+			}
 
-        } catch (SQLException e) {
-		System.out.println(e.getMessage());
-		e.printStackTrace();
-        }
-    }
+			ps.close();
+			rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return broj;
+	}
 
-    private int checkFakturaExist() {
-        int broj = 1;
-        PreparedStatement ps;
-        ResultSet rs;
-        String query = "SELECT * FROM fakture WHERE userID=? AND datum=?";
-
-        try {
-            ps = db.conn.prepareStatement(query);
-            ps.setInt(1, userID);
-            ps.setString(2, this.zaGodinu.format(DateTimeFormatter.ofPattern("yyyy-MM")));
-            rs = ps.executeQuery();
-
-            if (rs.isBeforeFirst()) {
-                broj = Integer.valueOf(rs.getString("brojFakture"));
-                idFakture = rs.getInt("id");
-            }
-
-            ps.close();
-            rs.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return broj;
-    }
-
-    public void createFakturu(ResultSet rs, database db) {
-        if (brFakture > 1) {
-            insertFaktureData(rs);
-        } else {
-            insertFaktura();
-            brFakture = checkFakturaExist();
-            insertFaktureData(rs);
-        }
-    }
+	public void createFakturu(ResultSet rs, database db) {
+		insertFaktureData(rs);
+	}
 }
--- src/main/java/net/yuvideo/jgemstone/server/classes/monthlyScheduler.java
+++ src/main/java/net/yuvideo/jgemstone/server/classes/monthlyScheduler.java
@@ -5,126 +5,149 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  * Created by zoom on 9/9/16.
  */
 public class monthlyScheduler {
-    public database db;
-    private SimpleDateFormat format_first_day_in_month = new SimpleDateFormat("yyyy-MM-01");
-    private SimpleDateFormat forma_normal_date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    private SimpleDateFormat format_date = new SimpleDateFormat("yyyy-MM-dd");
-    private SimpleDateFormat format_month = new SimpleDateFormat("yyyy-MM");
-    private Users users;
-    private ArrayList<Users> usersArrayList = new ArrayList<>();
-    private PreparedStatement ps;
-    private PreparedStatement psUpdateDebts;
-    private ResultSet rs;
-    private ResultSet rsUpdateDebts;
 
-    private List id_service = new ArrayList();
-    private Double ukupna_cena = 0.00;
+	public database db;
+	private SimpleDateFormat format_first_day_in_month = new SimpleDateFormat("yyyy-MM-01");
+	private SimpleDateFormat forma_normal_date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	private SimpleDateFormat format_date = new SimpleDateFormat("yyyy-MM-dd");
+	private SimpleDateFormat format_month = new SimpleDateFormat("yyyy-MM");
+	private Users users;
+	private ArrayList<Users> usersArrayList = new ArrayList<>();
+	private PreparedStatement ps;
+	private PreparedStatement psUpdateDebts;
+	private ResultSet rs;
+	private ResultSet rsUpdateDebts;
 
-    private user_debts userDebt;
-    private ArrayList<user_debts> userDebts;
+	private List id_service = new ArrayList();
+	private Double ukupna_cena = 0.00;
 
-    private final Logger LOGGER = Logger.getLogger("MONTHLY_SCHEDULER");
+	private user_debts userDebt;
+	private ArrayList<user_debts> userDebts;
 
-    private String query;
+	private final Logger LOGGER = Logger.getLogger("MONTHLY_SCHEDULER");
 
-    public void monthlyScheduler() {
-        query = "SELECT *  FROM servicesUser WHERE obracun=1 AND aktivan=1 AND linkedService=0  ";
-        //koji je mesec zaduzenja. posto je sada novi mesec kada se zaduzuje korisnik onda idemo mesec dana u nazad.
-        //obracun je za prosli mesec
+	private String query;
 
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.MONTH, -1);
-        try {
-            ps = db.conn.prepareStatement(query);
-            rs = ps.executeQuery();
-            if (rs.isBeforeFirst()) {
-                while (rs.next()) {
-                    query = "INSERT INTO userDebts (id_ServiceUser, nazivPaketa, datumZaduzenja, userID, popust, paketType, cena, dug, zaMesec, PDV)" +
-                            "VALUES " +
-                            "(?,?,?,?,?,?,?,?,?,?)";
-                    psUpdateDebts = db.conn.prepareStatement(query);
-                    psUpdateDebts.setInt(1, rs.getInt("id"));
-                    psUpdateDebts.setString(2, rs.getString("nazivPaketa"));
-                    psUpdateDebts.setDate(3, java.sql.Date.valueOf(format_first_day_in_month.format(new Date())));
-                    psUpdateDebts.setInt(4, rs.getInt("userID"));
-                    psUpdateDebts.setDouble(5, rs.getDouble("popust"));
-                    psUpdateDebts.setString(6, rs.getString("paketType"));
-                    psUpdateDebts.setDouble(7, rs.getDouble("cena"));
-                    psUpdateDebts.setDouble(8, valueToPercent.getValue(rs.getDouble("cena"), rs.getDouble("popust")));
-                    psUpdateDebts.setString(9, format_month.format(cal.getTime()));
-                    psUpdateDebts.setDouble(10, rs.getDouble("pdv"));
-                    if (!rs.getBoolean("newService")) {
-                        //ako servis je vec zaduzen onda preskociti zaduzenje od strane servera :)
-                        if (!check_skip_userDebt(rs.getInt("id"), rs.getInt("userID"), format_month.format(cal.getTime())))
-                            psUpdateDebts.executeUpdate();
-                    } else {
-                        setOldService(rs.getInt("id"));
-                    }
-                    LocalDate date = LocalDate.of(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
-                    zaduziFakturu(rs.getInt("userID"), date, "SYSTEM", rs);
-                }
-            }
-            rs.close();
-            psUpdateDebts.close();
-            ps.close();
-            psUpdateDebts.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+	public void monthlyScheduler() {
+		LocalDate date = null;
+		int userID = 0;
+		query = "SELECT *  FROM servicesUser WHERE obracun=1 AND aktivan=1 AND linkedService=0  ";
+		//koji je mesec zaduzenja. posto je sada novi mesec kada se zaduzuje korisnik onda idemo mesec dana u nazad.
+		//obracun je za prosli mesec
 
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.MONTH, -1);
+		try {
+			ps = db.conn.prepareStatement(query);
+			rs = ps.executeQuery();
+			if (rs.isBeforeFirst()) {
+				while (rs.next()) {
+					query = "INSERT INTO userDebts (id_ServiceUser, nazivPaketa, datumZaduzenja, userID, popust, paketType, cena, dug, zaMesec, PDV)"
+							+ "VALUES "
+							+ "(?,?,?,?,?,?,?,?,?,?)";
+					psUpdateDebts = db.conn.prepareStatement(query);
+					psUpdateDebts.setInt(1, rs.getInt("id"));
+					psUpdateDebts.setString(2, rs.getString("nazivPaketa"));
+					psUpdateDebts.setDate(3, java.sql.Date.valueOf(format_first_day_in_month.format(new Date())));
+					psUpdateDebts.setInt(4, rs.getInt("userID"));
+					psUpdateDebts.setDouble(5, rs.getDouble("popust"));
+					psUpdateDebts.setString(6, rs.getString("paketType"));
+					psUpdateDebts.setDouble(7, rs.getDouble("cena"));
+					psUpdateDebts.setDouble(8, valueToPercent.getValue(rs.getDouble("cena"), rs.getDouble("popust")));
+					psUpdateDebts.setString(9, format_month.format(cal.getTime()));
+					psUpdateDebts.setDouble(10, rs.getDouble("pdv"));
+					if (!rs.getBoolean("newService")) {
+						//ako servis je vec zaduzen onda preskociti zaduzenje od strane servera :)
+						if (!check_skip_userDebt(rs.getInt("id"), rs.getInt("userID"), format_month.format(cal.getTime()))) {
+							psUpdateDebts.executeUpdate();
+						}
+					} else {
+						setOldService(rs.getInt("id"));
+					}
+					date = LocalDate.of(cal.getInstance().get(Calendar.YEAR), cal.getInstance().get(Calendar.MONTH), cal.getInstance().get(Calendar.DAY_OF_MONTH));
+					userID = rs.getInt("userID");
+				}
+			}
+			rs.close();
+			psUpdateDebts.close();
+			ps.close();
+			psUpdateDebts.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		zaduziFakturu(userID, date, "SYSTEM");
 
-    }
+	}
 
+	private void zaduziFakturu(int userID, LocalDate godina, String operater) {
+		System.out.println("zaduzivnje useara:"+userID);
+		try {
+			PreparedStatement ps;
+			ResultSet rs = null;
+			String query = "SELECT * FROM userDebts WHERE userID=? and zaMesec=?";
 
-    private void zaduziFakturu(int userID, LocalDate godina, String operater, ResultSet rs) {
-        FaktureFunct faktureFunct = new FaktureFunct(userID, godina, operater, db);
-        if (faktureFunct.hasFirma) {
-            faktureFunct.createFakturu(rs, db);
-        }
+			ps = db.conn.prepareStatement(query);
+			ps.setInt(1, userID);
+			ps.setString(2, godina.format(DateTimeFormatter.ofPattern("yyyy-MM")));
+			rs = ps.executeQuery();
+			System.out.println("user godia: "+userID+" "+godina.format(DateTimeFormatter.ofPattern("yyyy-MM")));
+			if (rs.isBeforeFirst()) {
+				while (rs.next()) {
+					FaktureFunct faktureFunct = new FaktureFunct(userID, godina, operater, db);
+					if (faktureFunct.hasFirma) {
+						System.out.println("userFIRMA: "+faktureFunct.hasFirma);
+						faktureFunct.createFakturu(rs, db);
+					}
+				}
+			}
+		} catch (SQLException ex) {
+			Logger.getLogger(monthlyScheduler.class.getName()).log(Level.SEVERE, null, ex);
+		}
 
-    }
+	}
 
-    private void setOldService(int id) {
-        query = "UPDATE servicesUser set newService=0 WHERE id=?";
-        try {
-            PreparedStatement ps2 = db.conn.prepareStatement(query);
-            ps2.setInt(1, id);
-            ps2.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+	private void setOldService(int id) {
+		query = "UPDATE servicesUser set newService=0 WHERE id=?";
+		try {
+			PreparedStatement ps2 = db.conn.prepareStatement(query);
+			ps2.setInt(1, id);
+			ps2.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
-    private Boolean check_skip_userDebt(int id_service, int userID, String zaMesec) {
-        PreparedStatement psCheck;
-        ResultSet rsCheck;
-        Boolean check = false;
+	private Boolean check_skip_userDebt(int id_service, int userID, String zaMesec) {
+		PreparedStatement psCheck;
+		ResultSet rsCheck;
+		Boolean check = false;
 
-        String queryCheck = "SELECT * FROM userDebts WHERE id_ServiceUser=? AND userID=? and zaMesec=?";
-        try {
-            psCheck = db.conn.prepareStatement(queryCheck);
-            psCheck.setInt(1, id_service);
-            psCheck.setInt(2, userID);
-            psCheck.setString(3, zaMesec);
-            rsCheck = psCheck.executeQuery();
-            check = rsCheck.isBeforeFirst();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+		String queryCheck = "SELECT * FROM userDebts WHERE id_ServiceUser=? AND userID=? and zaMesec=?";
+		try {
+			psCheck = db.conn.prepareStatement(queryCheck);
+			psCheck.setInt(1, id_service);
+			psCheck.setInt(2, userID);
+			psCheck.setString(3, zaMesec);
+			rsCheck = psCheck.executeQuery();
+			check = rsCheck.isBeforeFirst();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
-        return check;
-    }
-
+		return check;
+	}
 
 }
-
