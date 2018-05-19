550e118acfa71b9ba65e107473b64ec0703e35f1
PsyhoZOOM <PsyhoZOOM@gmail.com>
2018-05-19 18:17:18 +0200

    get value of perc and get perc value of sum

--- src/main/java/net/yuvideo/jgemstone/server/classes/valueToPercent.java
+++ src/main/java/net/yuvideo/jgemstone/server/classes/valueToPercent.java
@@ -6,31 +6,13 @@ package net.yuvideo.jgemstone.server.classes;
  */
 public class valueToPercent {
 
-
-  public static Double getValue(Double value, double percentage) {
-    double percent = value - (value * percentage / 100);
-    return percent;
-
+  public static Double getPDVOfValue(double value, double perc){
+    double pdv = value * perc / 100;
+    return  pdv;
   }
 
-  public static Double getPercentage(double value, Double oFvalue) {
-    double percentage = value / oFvalue;
-    double result = percentage * 100;
-    return result;
-  }
-
-  public static Double getDiffValue(double value, double pdv) {
-    double percen = value - (value * pdv / 100);
-    return value - percen;
-  }
-
-  public static Double getValueOfPercentAdd(double value, double perc) {
-    double result = (perc / 100 + 1) * value - value;
-    return result;
-  }
-
-  public static Double getValueOfPercentSub(double value, double perc) {
-    double result = value - (value / (perc / 100 + 1));
-    return result;
+  public static Double getPDVOfSum(double value, double perc){
+    double pdv = (value * perc) / (100 + perc);
+    return pdv ;
   }
 }
