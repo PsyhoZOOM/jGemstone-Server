a5394139923117da22b3eafaf36426fd1a7b3373
PsyhoZOOM <psyhozoom@gmail.com>
2018-05-21 14:51:21 +0200

    deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/Server.class deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/ARTIKLI/ArtikliFunctions.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/BOX/addBoxService.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/ClientWorker.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/DTV/DTVFunctions.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/EMMServer.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/FIX/CSVData.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/FIX/FIXFunctions.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/FaktureFunct.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/Groups.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/INTERNET/NETFunctions.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/IPTV/IPTVFunctions.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/IPTV/StalkerRestAPI2.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/MESTA/MestaFuncitons.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/MISC/mysqlMIsc.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/OBRACUNI/MesecniObracun.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/RACUNI/RacunFaktura.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/RACUNI/UserRacun.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/SERVICES/ServicesFunctions.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/SchedullerTask.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/Services.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/USERS/UserFunc.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/USERS/UsersData.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/Uplate.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/Users.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/WiFiTracker.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/database.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/md5_digiest.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/messageS.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/monthlyScheduler.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/sendEMMUDP.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/user_debts.class 	deleted:    build/classes/java/main/net/yuvideo/jgemstone/server/classes/valueToPercent.class 	deleted:    build/classes/java/main/ssl 	deleted:    build/distributions/jGemstone-Server.tar 	deleted:    build/distributions/jGemstone-Server.zip 	deleted:    build/libs/jGemstone-Server.jar 	deleted:    build/resources/main/ssl/clientpub.cer 	deleted:    build/resources/main/ssl/clientpub.jks 	deleted:    build/resources/main/ssl/generate.sh 	deleted:    build/resources/main/ssl/plainclient.jks 	deleted:    build/resources/main/ssl/plainserver.jks 	deleted:    build/resources/main/ssl/serverpub.cer 	deleted:    build/resources/main/ssl/serverpub.jks 	deleted:    build/scripts/jGemstone-Server 	deleted:    build/scripts/jGemstone-Server.bat 	deleted:    build/tmp/jar/MANIFEST.MF

--- build/resources/main/ssl/generate.sh
+++
@@ -1,9 +0,0 @@
-echo "Generate the Client and Server Keystores" > /dev/null
-keytool -genkeypair -alias plainserverkeys -keyalg RSA -dname "CN=Plain Server,OU=kl2217,O=kl2217org,L=Boston,ST=MA,C=US" -keypass jgemstone -keystore plainserver.jks -storepass jgemstone
-keytool -genkeypair -alias plainclientkeys -keyalg RSA -dname "CN=Plain Client,OU=kl2217,O=kl2217org,L=Boston,ST=MA,C=US" -keypass jgemstone -keystore plainclient.jks -storepass jgemstone
-echo "Export the server public certificate and create a seperate keystore">/dev/null
-keytool -exportcert -alias plainserverkeys -file serverpub.cer -keystore plainserver.jks -storepass jgemstone
-keytool -importcert -keystore serverpub.jks -alias serverpub -file serverpub.cer -storepass jgemstone
-echo "Export the client public certificate and create a seperate keystore">/dev/null
-keytool -exportcert -alias plainclientkeys -file clientpub.cer -keystore plainclient.jks -storepass jgemstone
-keytool -importcert -keystore clientpub.jks -alias clientpub -file clientpub.cer -storepass jgemstone
