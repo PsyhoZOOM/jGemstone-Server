0aa28fdde4e205ff31b8fb5db9f5bf437234ca01
PsyhoZOOM <psyhozoom@gmail.com>
2018-06-07 15:09:16 +0200

    added MirkotikAPI

---
+++ src/main/java/me/legrange/mikrotik/impl/Done.java
@@ -0,0 +1,24 @@
+package me.legrange.mikrotik.impl;
+
+/**
+ * Internal representation of !done
+ *
+ * @author GideonLeGrange
+ */
+class Done extends Response {
+
+  Done(String tag) {
+    super(tag);
+  }
+
+  void setHash(String hash) {
+    this.hash = hash;
+  }
+
+  String getHash() {
+    return hash;
+  }
+
+  private String hash;
+
+}
