7a66ba7f590b59da184b3dace19aec1d0e1dd9b6
PsyhoZOOM <psyhozoom@gmail.com>
2019-02-27 14:33:01 +0100

    v0.212      izvestaji uplata

--- src/main/java/net/yuvideo/jgemstone/server/classes/ClientWorker.java
+++ src/main/java/net/yuvideo/jgemstone/server/classes/ClientWorker.java
@@ -55,7 +55,7 @@ import org.json.JSONObject;
 public class ClientWorker implements Runnable {
 
   public Logger LOGGER;
-  private static final String S_VERSION = "0.211";
+  private static final String S_VERSION = "0.212";
   private String C_VERSION;
   private final SimpleDateFormat date_format_full = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
   private final SimpleDateFormat mysql_date_format = new SimpleDateFormat("yyy-MM-dd hh:mm:ss");
